981a6e4c72f841152bf50c1b765d14c9972e8b23
Eric <egant43123@gmail.com>
2021-07-15 23:16:18 -0400

    Updates

---
+++ precache-manifest.05352ca4958836e0918d7a89560a92a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dabd8270360f04e13734cbca351184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "288ea716774cb31efc89",
+    "url": "/static/css/2.adcc7399.chunk.css"
+  },
+  {
+    "revision": "b053071f69766c5e57ce",
+    "url": "/static/css/main.c898e1a1.chunk.css"
+  },
+  {
+    "revision": "288ea716774cb31efc89",
+    "url": "/static/js/2.6f131d38.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.6f131d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b053071f69766c5e57ce",
+    "url": "/static/js/main.1304928f.chunk.js"
+  },
+  {
+    "revision": "296ca7f0f95529e3a811",
+    "url": "/static/js/runtime-main.1840241c.js"
+  }
+]);
