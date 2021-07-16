3545242740067c8d8a50a2e395e9b0e8aded9142
Eric <egant43123@gmail.com>
2021-07-15 23:57:53 -0400

    Updates

--- precache-manifest.8108c10eb88d72142dcc021a6c610577.js
+++ precache-manifest.8108c10eb88d72142dcc021a6c610577.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21b5dec258d34eeb1f966d4ba8da9e2a",
+    "revision": "773d0d7343a4f42b79a294bf961b21de",
     "url": "/EricGant/nasa-photo-of-the-day/index.html"
   },
   {
-    "revision": "995508fcd308fabf33ea",
-    "url": "/EricGant/nasa-photo-of-the-day/static/css/2.adcc7399.chunk.css"
+    "revision": "a2c1a4dde9b3afc6dd6a",
+    "url": "/EricGant/nasa-photo-of-the-day/static/css/2.0322c69f.chunk.css"
   },
   {
     "revision": "b053071f69766c5e57ce",
     "url": "/EricGant/nasa-photo-of-the-day/static/css/main.c898e1a1.chunk.css"
   },
   {
-    "revision": "995508fcd308fabf33ea",
+    "revision": "a2c1a4dde9b3afc6dd6a",
     "url": "/EricGant/nasa-photo-of-the-day/static/js/2.b391c0cc.chunk.js"
   },
   {
